dd3ae356d8c30ac1deec8b6afdeb9fbb71436d16
Marvin <chmarvin@protonmail.com>
2023-05-25 17:44:34 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width,initial-scale=1">
+    <title>Marvin Juraschka – Software Developer</title>
+    <meta name="description" content="Marvin Juraschka - Software Developer">
+
+    <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>👨‍💻</text></svg>">
+    <link href="https://fonts.googleapis.com/css?family=Overpass" rel="stylesheet">
+    <link rel="stylesheet" href="/assets/css/style.css">
+
+    <script defer src="https://use.fontawesome.com/releases/v5.7.2/js/all.js" integrity="sha384-0pzryjIRos8mFBWMzSSZApWtPl/5++eIfzYmTgBBmXYdhvxPc+XcFEk+zJwDgWbP" crossorigin="anonymous"></script>
+
+    <meta name="msapplication-TileColor" content="#000000">
+    <meta name="theme-color" content="#000000">
+</head>
+<body>
+<div id="page">
+    <div id="wrapper">
+        <div id="box">
+            <div id="header" class="padded">
+                <img src="/assets/img/header.svg" alt="header" height="50">
+            </div>
+            <div id="about" class="padded">
+                <h1>Marvin Juraschka</h1>
+                <h2>Software Developer</h2>
+            </div>
+            <div id="social" class="padded">
+                <a href="mailto:info@ccmvn.co" title="Email"><i class="fas fa-envelope"></i></a>
+                <a href="https://www.linkedin.com/in/marvin-juraschka-05b840270/" title="LinkedIn"><i class="fab fa-linkedin"></i></a>
+                <a href="https://github.com/ccmvn/" title="GitHub"><i class="fab fa-github"></i></a>
+            </div>
+        </div>
+        <div id="legal" class="padded">
+            Copyright &copy; All rights reserved
+        </div>
+    </div>
+</div>
+</body>
+</html>
